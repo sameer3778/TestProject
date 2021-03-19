39f9181e089dba79a205085b7f5f944e8c10747b
schavan <schavan@SAMEERC-PC99.aurustech.com>
2021-03-19 17:24:56 +0530

    Addition

---
+++ TestProjectJava/src/BasicJava/Addition.java
@@ -0,0 +1,15 @@
+package BasicJava;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int x=20;
+		int  y=20;
+		
+		int c = x+y; ;
+		
+		System.out.println("addition is: "+c);
+
+	}
+
+}
